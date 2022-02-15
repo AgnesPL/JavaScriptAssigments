fd225396a7fd794d7094db1e00d9ca1a744e9368
AgnesPL <98382860+AgnesPL@users.noreply.github.com>
2022-02-15 13:54:00 +0100

    Add files via upload

---
+++ operators/Operators.js
@@ -0,0 +1,89 @@
+'use strict';
+
+//assignemnt 1
+/*
+let numOfDaysUntilWeekend = 2;
+numOfDaysUntilWeekend -= 1;
+
+
+1. Use console.log() to log the value just after the variable has been declared
+2.  Use console.log() to log the value right after the value of the variable has been changed. 
+3. Tell what it does 
+*/
+let numOfDaysUntilWeekend = 2;
+console.log(numOfDaysUntilWeekend);
+
+//assignemnt 2
+//Addition to numbers
+let numOfDaysUntilWeekend = 2;
+console.log(numOfDaysUntilWeekend);
+numOfDaysUntilWeekend +=1;
+console.log(numOfDaysUntilWeekend);
+
+//assignemnt 3
+//Subtraction to numbers
+let numOfDaysUntilWeekend = 2;
+console.log(numOfDaysUntilWeekend);
+numOfDaysUntilWeekend -=1;
+console.log(numOfDaysUntilWeekend);
+
+//assignemnt 4
+//write two sentences in two variables and connect them together 
+let numOfDaysUntilWeekend = 2;
+console.log(numOfDaysUntilWeekend);
+numOfDaysUntilWeekend +=1;
+numOfDaysUntilWeekend -=1;
+console.log(numOfDaysUntilWeekend);
+
+//assignemnt 5
+//write two variables, one number and one string. add them together
+let x = 25;
+var g = 30;
+console.log (a+g);
+
+//assignemnt 6
+//write the answears in the in a comment
+let r = 15;
+let s = 5;
+let t = r + s; //20
+let u = r - s; //10
+let v = t / u; //2
+let x = r * v; //30
+let y = s % v; //1
+
+//assignemnt 7
+//Why does it not write out? 
+let r = 34;
+let c = 1;
+let x = r - 32 + c;
+let y = r++ * 4; 
+if(x == 3 || y == 140 ){ //the numbers in "if" section were uncorrect
+ console.log("yeah you got it");
+}
+//change a number in a variable, so it print out the text
+
+
+//assignemnt 8
+//make two variables. One variable must contain a number and the other one must contain a prompt, 
+//where the user can enter a number. Match the two number and print out the answear
+//TIP: search prompt
+let x = 50;
+var z = prompt("Enter a value","0");
+console.log (x+z); //if z=8 output is 508
+
+//assignment 9
+//write these numbers in the console and write the output. Explain you thing this happens
+    0.1 + 0.2; //sum 0.1 and 0.2
+    0.1 + 0.2 === 0.3; //confirm, that sum of 0.1 and 0.2 is 0.3 
+    3  - 1 
+    3  + 1
+    '3' - 1 
+    '3' + 1 
+    '222' - -'111' // the output is only the answer for the last operation, because the whole code were the mathematical operations, we got an answer for the last one
+//output=333
+
+//assignemnt 9
+//Write a JavaScript program to determine whether a given year is a leap year 
+
+
+
